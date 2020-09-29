a1bef03dad1700e6ba33f1b40059ae969bfaecfd
Tanya <tanyanaumik@tut.by>
2020-09-29 15:15:34 +0300

    All 5 task

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -4,6 +4,7 @@
  * @returns {string}
  */
 function ucFirst(str) {
-  let str1 = str[0].toUpperCase();
+  let str1 = str.charAt(0).toUpperCase();
   str = str1 + str.slice(1);
+  return str;
 }
--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,5 +5,12 @@
  * @returns {string}
  */
 function truncate(str, maxlength) {
-  // ваш код...
+  if(str.length > maxlength){
+    let trancatStr = str.slice(0,maxlength-1) + '…';
+    return trancatStr;
+  }else{
+    return str;
+  }
+  
 }
+
